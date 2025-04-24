3612e5abdcb74c0f9701318749ff2a93056bc4ca
j-chau <joeymchau@gmail.com>
2025-04-24 07:53:43 -0700

    Add config files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,6 @@
+alias pom=pnpm
+alias cat=bat
+
+alias ls='colorls --group-directories-first'
+
+alias loh='cd ~/Documents/optimizelabs/monarch-fork/'
---
+++ zsh/config.zsh
@@ -0,0 +1,18 @@
+
+source $ZSH_CUSTOM/catppuccin_macchiato-zsh-syntax-highlighting.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+
+RUBY="/opt/homebrew/opt/ruby/bin:"
+GO="$HOME/go/bin:"
+export PATH="$RUBY:$GO:$PATH"
+
+PATH=$PATH:$(ruby -e 'puts Gem.bindir')
+
+eval "$(thefuck --alias FUCK)"
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /Users/joeychau/.config/broot/launcher/bash/br
